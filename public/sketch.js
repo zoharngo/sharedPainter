d2a610998e9c573eaac6950f9cc506d31b7a5c0d
Zohar Nyego <zoharngo@gmail.com>
2018-01-18 15:30:40 -0800

    removing canvas p5 library and using native JS code

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,47 @@
-var socket;
 
-function setup() {
-    createCanvas(600, 400);
-    background(51);
+var socket = io.connect('http://localhost:3000');
 
-    socket = io.connect('http://localhost:3000');
-    socket.on('draw', data => {
-        noStroke();
-        fill(data.color.r, data.color.g, data.color.b);
-        ellipse(data.x, data.y, 5, 5);
-    });
+window.onload = () => {
+    setup();
 }
 
-function mouseDragged() {
-    console.log(mouseX + ',' + mouseY);
-    noStroke();
-    fill(255,0,100);
-    ellipse(mouseX, mouseY, 5, 5);
-    socket.emit('draw', { x: mouseX, y: mouseY, color: { r: 255, g: 0, b: 100 } });
+function setup() {
+    let mode = false;
+    let canvas = document.getElementById('painter');
+    var ctx = canvas.getContext('2d');
+
+    canvas.addEventListener('mousedown', () => {
+        mode = true;
+    });
+    canvas.addEventListener('mouseup', () => {
+        mode = false;
+        canvas.style.cursor = 'default';
+    });
+    canvas.addEventListener('mousemove', (e) => {
+        var cordinates = getMouseCords(canvas, e);
+        canvas.style.cursor = 'pointer';
+        draw(ctx, cordinates.x, cordinates.y, mode);
+    });
+
+    socket.on('draw', data => {
+        canvas.style.cursor = 'pointer';
+        draw(ctx, data.x, data.y, true);
+    });
+
+}
+
+function getMouseCords(canvas, event) {
+    var boundingRect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - boundingRect.left,
+        y: event.clientY - boundingRect.top
+    }
+}
+
+function draw(ctx, x, y, mode) {
+    if (mode) {
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(x, y, 4, 4);
+        socket.emit('draw', { x: x, y: y, color: '#FF0000' });
+    }
 }
